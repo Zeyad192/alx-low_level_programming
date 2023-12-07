9b8f257af23e6f254aed8ab498c682eebd4eed6d
zeyad192 <zeyad.assem@live.com>
2023-12-07 19:23:34 +0200

    a

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,11 +18,20 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct listint_s
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
